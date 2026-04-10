b5a2c0aaf1669010e454a2b7a1cbbb6974b3c8c7
Roy <shuwj81@daum.net>
2026-04-10 22:41:30 +0900

    NestedCoordinator: body 직접 작성 예제
    - @FlowCoordinator(screen:) 유지 → State 자동 생성
    - Action 직접 작성 → 매크로 건너뜀
    - body 직접 작성 → 매크로 건너뜀, .forEachRoute 수동
    - DemoCoordinator는 handleRoute 방식 (자동)

--- Example/TCAFlowExamples/TCAFlowExamples/Features/Nested/NestedCoordinator.swift
+++ Example/TCAFlowExamples/TCAFlowExamples/Features/Nested/NestedCoordinator.swift
@@ -1,12 +1,42 @@
 import ComposableArchitecture
 import TCAFlow
 
+// MARK: - NestedCoordinator (body 직접 작성 예제)
+
 @FlowCoordinator(screen: "NestedScreen", navigation: true)
 struct NestedCoordinator {
   @CasePathable
   enum Action {
     case router(IndexedRouterActionOf<NestedScreen>)
     case backToMain
+  }
+
+  // body 직접 작성 → 매크로가 body 생성 건너뜀
+  var body: some Reducer<State, Action> {
+    Reduce { state, action in
+      switch action {
+      case .router(.routeAction(_, .step1(.nextStep))):
+        state.routes.push(.step2(.init()))
+        return .none
+
+      case .router(.routeAction(_, .step1(.backToMain))):
+        return .send(.backToMain)
+
+      case .router(.routeAction(_, .step2(.goBack))):
+        state.routes.goBack()
+        return .none
+
+      case .router(.routeAction(_, .step2(.finish))):
+        return .send(.backToMain)
+
+      case .backToMain:
+        return .none
+
+      default:
+        return .none
+      }
+    }
+    .forEachRoute(\.routes, action: \.router)
   }
 }
 
@@ -19,29 +49,3 @@ extension NestedCoordinator {
 }
 
 extension NestedCoordinator.NestedScreen.State: Equatable {}
-
-extension NestedCoordinator {
-  func handleRoute(state: inout State, action: Action) -> Effect<Action> {
-    switch action {
-    case .router(.routeAction(_, .step1(.nextStep))):
-      state.routes.push(.step2(.init()))
-      return .none
-
-    case .router(.routeAction(_, .step1(.backToMain))):
-      return .send(.backToMain)
-
-    case .router(.routeAction(_, .step2(.goBack))):
-      state.routes.goBack()
-      return .none
-
-    case .router(.routeAction(_, .step2(.finish))):
-      return .send(.backToMain)
-
-    case .backToMain:
-      return .none
-
-    default:
-      return .none
-    }
-  }
-}
